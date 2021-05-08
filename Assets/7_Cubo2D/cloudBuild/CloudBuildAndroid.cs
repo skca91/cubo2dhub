5524b7e18b423caca0cc8d76db80280bbf91d19e
Daniel <danielp299@gmail.com>
2021-05-08 18:46:35 -0300

    pakage old

---
+++ Assets/7_Cubo2D/cloudBuild/CloudBuildAndroid.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class CloudBuildAndroid : MonoBehaviour
+{
+    /// <summary>
+    /// Puede variar dependiendo de la version de udp
+    /// </summary>
+    public static readonly string rutaBillingModeJson = @"Assets/Resources/BillingMode.json";
+
+    private static CloudBuildAndroid cloudBuildInstance;
+
+    public enum Cubo2dBillingMode
+    {
+        UDP,
+        GooglePlay,
+        SamsungApps,
+        AmazonAppStore
+    }
+
+    public static CloudBuildAndroid GetInstance()
+    {
+        if (cloudBuildInstance == null)
+        {
+            cloudBuildInstance = new CloudBuildAndroid();
+        }
+        return cloudBuildInstance;
+    }
+
+    /// <summary>
+    /// El BillingMode del Editor debe estar en modo UDP para que se aplique la configuracion.
+    /// </summary>
+    public void SetBillingMode(Cubo2dBillingMode BillingMode, string ruta)
+    {
+
+        Debug.Log("SetBillingMode: " + BillingMode.ToString());
+        Debug.Log("rutaBillingModeJson: " + ruta.ToString());
+
+        string json = File.ReadAllText(ruta);
+
+        if (string.IsNullOrEmpty(json))
+        {
+            Debug.LogError("No se el json de billing en: " + ruta.ToString());
+            return;
+        }
+
+        switch (BillingMode)
+        {
+            case Cubo2dBillingMode.GooglePlay:
+                json = json.Replace("UDP", "GooglePlay");
+                break;
+            case Cubo2dBillingMode.SamsungApps:
+                json = json.Replace("UDP", "SamsungApps");
+                break;
+            case Cubo2dBillingMode.AmazonAppStore:
+                json = json.Replace("UDP", "AmazonAppStore");
+                break;
+            case Cubo2dBillingMode.UDP:
+                break;
+            default:
+                break;
+        }
+
+
+        File.WriteAllText(ruta, json);
+
+        Debug.Log("Prebuild end ok");
+    }
+}
---
+++ Assets/7_Cubo2D/plataformas/sensorEnviaMensaje.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sensorEnviaMensaje : MonoBehaviour {
+
+	public enum modo
+	{
+		envia_a_hermano,
+		envia_a_padre,
+		envia_a_hijo
+	}
+
+	public modo v_modo = modo.envia_a_hermano;
+	public string mensajeEnter= "nada";
+	public string mensajeExit = "nada";
+	/**Si la espera es mayor a 0 espera*/
+	public float v_espera = 0;
+	public float v_frecuenciaMinima = 0;
+	float v_inicioEsperaEnter = 0;
+	bool v_estaEsperandoEnter = false;
+	float v_inicioEsperaExit = 0;
+	bool v_estaEsperandoExit = false;
+	float v_inicioFrecuenciaMinima = 0;
+	public string filtroPorTag = "Player";
+	//public tag
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(activarConEsperaEnter ()){
+
+			if (mensajeEnter.Length < 1)
+				return;
+
+			if(v_modo == modo.envia_a_hermano){
+				SendMessage (mensajeEnter);
+			}else if(v_modo == modo.envia_a_padre){
+				SendMessageUpwards (mensajeEnter);
+			}else if(v_modo == modo.envia_a_hijo){
+				BroadcastMessage (mensajeEnter);
+			}
+
+		}
+
+		if(activarConEsperaExit ()){
+
+			if (mensajeExit.Length < 1)
+				return;
+
+			if(v_modo == modo.envia_a_hermano){
+				SendMessage (mensajeExit);
+			}else if(v_modo == modo.envia_a_padre){
+				SendMessageUpwards (mensajeExit);
+			}else if(v_modo == modo.envia_a_hijo){
+				BroadcastMessage (mensajeExit);
+			}
+		}
+	}
+
+	public void nada(){
+	
+	}
+
+	public bool activarConEsperaEnter(){
+		if (v_estaEsperandoEnter && Time.time > v_inicioEsperaEnter + v_espera) {
+			v_estaEsperandoEnter = false;
+			return true;
+		}
+		return false;
+	}
+
+	/*public void reset(){
+		v_estaEsperandoEnter = false;
+		v_estaEsperandoExit = false;
+	}*/
+
+	public bool activarConEsperaExit(){
+		if (v_estaEsperandoExit && Time.time > v_inicioEsperaExit + v_espera) {
+			v_estaEsperandoExit = false;
+			return true;
+		}
+		return false;
+	}
+
+	public void OnTriggerEnter(Collider other){
+
+//		Debug.Log ("Enter: " +other.name);
+
+		if(other.gameObject.tag.Equals(filtroPorTag)){
+			Debug.Log ("OnTriggerEnter: " +other.name);
+			if (!v_estaEsperandoEnter && Time.time > v_inicioFrecuenciaMinima + v_frecuenciaMinima) {
+				v_inicioEsperaEnter = Time.time;
+				v_inicioFrecuenciaMinima = Time.time;
+				v_estaEsperandoEnter = true;
+			}
+		}
+	}
+
+	public void OnTriggerExit(Collider other){
+		if(other.gameObject.tag.Equals(filtroPorTag)){
+			if (!v_estaEsperandoExit && Time.time > v_inicioFrecuenciaMinima + v_frecuenciaMinima) {
+				v_inicioEsperaExit = Time.time;
+				v_inicioFrecuenciaMinima = Time.time;
+				v_estaEsperandoExit = true;
+			}
+		}
+	}
+
+
+}
